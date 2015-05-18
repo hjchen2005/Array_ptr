c4d80d46c39e5cb5b9f630197caae05f8199ad9f
hjchen2005 <hjchen2005@gmail.com>
2015-05-18 13:14:42 -0400

    Update clear_array_index.cpp

--- clear_array_index.cpp
+++ clear_array_index.cpp
@@ -1,14 +1,5 @@
-//
-//  clear_array_index.cpp
-//  Array_vs_pointer
-//
-//  Created by Hanjun Chen on 5/18/15.
-//  Copyright (c) 2015 Hanjun Chen. All rights reserved.
-//
+#include "clear_array_index.cpp"
 
-#include <cstdio>
-
-// Clears array using indexing.
 void ClearUsingIndex(int Array[],int size){
     int i;
     for
